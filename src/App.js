63762a3d83fcf15bf58712114791aba01e00cd7f
AgnivanshiR <86912784+AgnivanshiR@users.noreply.github.com>
2023-01-09 12:02:48 +0530

    my-fav-music-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  var [selectedGenre, setSelectedGenre] = useState("rock");
+
+  var musicDB = {
+    jazz: [
+      {
+        name: "My Way- Frank Sinatra",
+        rating: "❤️❤️❤️❤️❤️",
+        link: "https://music.youtube.com/watch?v=Qp6D71kQRhA"
+      },
+      {
+        name: "What A Wonderful World- Louis Armstrong",
+        rating: "❤️❤️❤️❤️❤",
+        link: "https://music.youtube.com/watch?v=VqhCQZaH4Vs"
+      }
+    ],
+  
+    rock: [
+      {
+        name: "In the End- linkin park",
+        rating: "❤️❤️❤️❤️❤️",
+        link: "https://music.youtube.com/watch?v=eVTXPUF4Oz4"
+      },
+      {
+        name: "Boulevard of Broken Dreams- Green Day",
+        rating: "❤️❤️❤️❤️❤",
+        link: "https://music.youtube.com/watch?v=Soa3gO7tL-c"
+      }
+    ],
+  
+    indian: [
+      {
+        name: "Khamoshiyan- Arjit Singh",
+        rating: "❤️❤️❤️❤️❤️",
+        link: "https://music.youtube.com/watch?v=Mv3SZDP7QUo"
+      },
+      {
+        name: "Moh Moh ke Dhage- Papon",
+        rating: "❤️❤️❤️❤️❤️",
+        link: "https://music.youtube.com/watch?v=X4rEsj4z7MY"
+      }
+    ]
+  };
+
+  function onClickHandler(genre) {
+    setSelectedGenre(genre);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Awesome Music</h1>
+      <small>checkout my favorite music, select a genre to get started</small>
+      <div>
+        {Object.keys(musicDB).map((genre) => (
+          <button onClick={() => onClickHandler(genre)}>{genre}</button>
+        ))}
+      </div>
+      <hr />
+
+      <div>
+        <ul>
+          {musicDB[selectedGenre].map((music) => (
+            <li>
+              <div>
+                <a href="music.link">{music.name}</a>
+              </div>
+              <div>{music.rating}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
